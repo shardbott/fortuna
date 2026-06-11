a6278997f3c9a465725b1757e72d8af737003a18
shardbott <gigantepipi@gmail.com>
2026-06-11 03:15:03 -0300

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth"
+import DiscordProvider from "next-auth/providers/discord"
+
+export const authOptions = {
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+}
+
+const handler = NextAuth(authOptions)
+export { handler as GET, handler as POST }
